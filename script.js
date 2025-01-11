bd8cad697dfd44703da69896a3c9dc024fa66500
DaDaDei <yuanhaiyuedadadei@gmail.com>
2025-01-11 22:50:48 +0800

    Initial commit

--- script.js
+++ script.js
@@ -4,3 +4,19 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+document.addEventListener("DOMContentLoaded", () => {
+  const textElement = document.querySelector("#text");
+  const text = "Xinyu Zhang"; 
+  let index = 0;
+
+  function type() {
+    if (index < text.length) {
+      textElement.textContent += text[index];
+      index++;
+      setTimeout(type, 150); 
+    }
+  }
+
+  type(); 
+});
